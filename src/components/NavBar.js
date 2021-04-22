a6ecc75ea7e8c4cf345d3c8033866f33acacc270
Nathan Fedzen <nfedzen@hotmail.com>
2021-04-22 15:30:42 -0600

    added header

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function NavBar(){
+
+    return(
+      <header>
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Products</h1>
+      </header>
+    )
+}
